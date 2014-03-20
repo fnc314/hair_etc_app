42e12579241682c64c0eb65b53518f157cc71dca
Franco <fnc314@gmail.com>
2014-03-20 13:30:53 -0700

    work on reservations controller with current_user

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,20 +1,35 @@
 class AppointmentsController < ApplicationController
+  
   before_filter :authenticate_client!
 
   def index
-    @appointments = current_client.appointments.all.includes(:services)
+    @appointments = current_client.appointments.all
   end
 
   def new
-    @appointment = current_client.appoinments.new
+    @appointment = current_client.appointments.new
   end
 
   def create
-    new_appointment = current_client.appointments.new
-    
+    new_appt_serv = params.require(:appointment)[:services]
+    new_appt = params.require(:appointment).permit(:appt_date_time)
+    appt = current_client.appointments.create
+    a_d_t = DateTime.new(new_appt["appt_date_time(1i)"].to_i,new_appt["appt_date_time(2i)"].to_i,
+      new_appt["appt_date_time(3i)"].to_i,new_appt["appt_date_time(4i)"].to_i, new_appt["appt_date_time(5i)"].to_i)
+    appt.appt_date_time = a_d_t
+    new_appt_serv.each do |s|
+      appt.services << Service.find(s.to_i) if s != ""
+    end
+    appt.stylist_id = params[:stylist].to_i
+    appt.save
+    respond_to do |f|
+      f.html { client_appointments_path(current_client.id) }
+    end
+
   end
 
   def show
+    @appointment = current_client.appointments.find(params[:id])
   end
 
   def update
@@ -22,4 +37,5 @@ class AppointmentsController < ApplicationController
 
   def destroy
   end
+
 end
