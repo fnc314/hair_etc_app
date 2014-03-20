d9cf0b11a871dce751957c78d8fab3339555d52c
Franco <fnc314@gmail.com>
2014-03-20 00:40:27 -0700

    nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,59 +1,47 @@
 HairEtcApp::Application.routes.draw do
   root to: 'clients#index'
+  
   devise_for :clients
-  resources :appointments
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  
+  resources :clients do
+    resources :appointments
+  end
+  
 end
+
+#                     Prefix Verb   URI Pattern                                         Controller#Action
+#                       root GET    /                                                   clients#index
+#         new_client_session GET    /clients/sign_in(.:format)                          devise/sessions#new
+#             client_session POST   /clients/sign_in(.:format)                          devise/sessions#create
+#     destroy_client_session DELETE /clients/sign_out(.:format)                         devise/sessions#destroy
+#            client_password POST   /clients/password(.:format)                         devise/passwords#create
+#        new_client_password GET    /clients/password/new(.:format)                     devise/passwords#new
+#       edit_client_password GET    /clients/password/edit(.:format)                    devise/passwords#edit
+#                            PATCH  /clients/password(.:format)                         devise/passwords#update
+#                            PUT    /clients/password(.:format)                         devise/passwords#update
+# cancel_client_registration GET    /clients/cancel(.:format)                           devise/registrations#cancel
+#        client_registration POST   /clients(.:format)                                  devise/registrations#create
+#    new_client_registration GET    /clients/sign_up(.:format)                          devise/registrations#new
+#   edit_client_registration GET    /clients/edit(.:format)                             devise/registrations#edit
+#                            PATCH  /clients(.:format)                                  devise/registrations#update
+#                            PUT    /clients(.:format)                                  devise/registrations#update
+#                            DELETE /clients(.:format)                                  devise/registrations#destroy
+#        client_confirmation POST   /clients/confirmation(.:format)                     devise/confirmations#create
+#    new_client_confirmation GET    /clients/confirmation/new(.:format)                 devise/confirmations#new
+#                            GET    /clients/confirmation(.:format)                     devise/confirmations#show
+#        client_appointments GET    /clients/:client_id/appointments(.:format)          appointments#index
+#                            POST   /clients/:client_id/appointments(.:format)          appointments#create
+#     new_client_appointment GET    /clients/:client_id/appointments/new(.:format)      appointments#new
+#    edit_client_appointment GET    /clients/:client_id/appointments/:id/edit(.:format) appointments#edit
+#         client_appointment GET    /clients/:client_id/appointments/:id(.:format)      appointments#show
+#                            PATCH  /clients/:client_id/appointments/:id(.:format)      appointments#update
+#                            PUT    /clients/:client_id/appointments/:id(.:format)      appointments#update
+#                            DELETE /clients/:client_id/appointments/:id(.:format)      appointments#destroy
+#                    clients GET    /clients(.:format)                                  clients#index
+#                            POST   /clients(.:format)                                  clients#create
+#                 new_client GET    /clients/new(.:format)                              clients#new
+#                edit_client GET    /clients/:id/edit(.:format)                         clients#edit
+#                     client GET    /clients/:id(.:format)                              clients#show
+#                            PATCH  /clients/:id(.:format)                              clients#update
+#                            PUT    /clients/:id(.:format)                              clients#update
+#                            DELETE /clients/:id(.:format)                              clients#destroy
