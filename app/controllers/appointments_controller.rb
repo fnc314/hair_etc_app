db047b7c2e7a732ca35a8bcf33c5d7835456eeca
Franco <fnc314@gmail.com>
2014-03-20 00:16:12 -0700

    added .env and secret key

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,12 +2,16 @@ class AppointmentsController < ApplicationController
   before_filter :authenticate_client!
 
   def index
+    @appointments = current_client.appointments.all.includes(:services)
   end
 
   def new
+    @appointment = current_client.appoinments.new
   end
 
   def create
+    new_appointment = current_client.appointments.new
+    
   end
 
   def show
