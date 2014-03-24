d719006e19d792724d03a8c3119679ec1c75938b
Franco <fnc314@gmail.com>
2014-03-23 18:44:27 -0700

    partialed out view for welcome screen to signed_in client

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < ApplicationController
 
   def index
+    if signed_in?
+      @appointment = current_client.appointments.order("updated_at").first
+      respond_to do |f|
+        f.html
+        f.json
+      end
+    else
+    end
   end
 
 end
