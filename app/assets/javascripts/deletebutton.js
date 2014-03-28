2c77ab7e31b4e30d11d74f7afef2825abafb1199
Franco <fnc314@gmail.com>
2014-03-27 21:31:26 -0700

    delete button on customers index page now removes appointment from view without reload and from database successfully

--- app/assets/javascripts/deletebutton.js
+++ app/assets/javascripts/deletebutton.js
@@ -1,21 +1,26 @@
 
 
 var deleteButton = function (current_client_id, app_id) {
+  // Prevent reload
   event.preventDefault();
-  
+  // Build Params hash in guise of token
   var authParam = $('meta[name=csrf-param]').attr('content');
   var authToken = $('meta[name=csrf-token]').attr('content');
   var data = {};
-  
+  // Add prebuilt authentication and passed in function arguments
+  // to params hash (data)
   data[authParam] = authToken;
   data.current_client = current_client_id.toString();
   data.id = app_id.toString();
-  
+  // Create ajax call to delete object and remove containing div from the page
+  // without refresh
   $.ajax({
     type: "delete",
     url: "/clients/" + current_client_id + "/appointments/" + app_id + '.json',
     data: data
-  }).success(function (current_client_id, app_id) {
-    console.log('#'+current_client_id+'_'+app_id);
+  }).success(function (response) {
+    var id1 = response.id.toString();
+    var ele = document.getElementById(id1);
+    ele.remove();
   });
 };
