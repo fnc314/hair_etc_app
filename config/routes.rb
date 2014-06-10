da7b41276870ef2fb339cf0560d87c2fa0673987
Franco <fnc314@gmail.com>
2014-06-10 16:49:59 -0400

    expanded api RESTful routes for new edits to Android views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 HairEtcApp::Application.routes.draw do
   
   namespace :api, defaults: {format: :json} do
-    resource :appointments, only: [:index, :create]
-    
+    resource :appointments, only: [:create]
+    get 'appointments', :to => 'appointments#index'
     resource :sessions, only: [:create]
   end
 
@@ -22,6 +22,7 @@ HairEtcApp::Application.routes.draw do
 end
 #                     Prefix Verb   URI Pattern                                         Controller#Action
 #           api_appointments POST   /api/appointments(.:format)                         api/appointments#create {:format=>:json}
+#                            GET    /api/appointments(.:format)                         api/appointments#index {:format=>:json}
 #               api_sessions POST   /api/sessions(.:format)                             api/sessions#create {:format=>:json}
 #                       root GET    /                                                   home#index
 #         new_client_session GET    /clients/sign_in(.:format)                          devise/sessions#new
