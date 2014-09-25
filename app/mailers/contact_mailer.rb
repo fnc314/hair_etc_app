22be1940e6a84fe01fd4c000a1f13a0725cfaddb
Franco N. Colaizzi <FrancoNColaizzi@fnc314.local>
2014-09-25 17:16:25 -0400

    added basic building blocks for ActionMailer and the email views

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,16 @@
+class ContactMailer < ActionMailer::Base
+  # Controller and methods used to initiate email
+
+  # Temporary Email
+
+  # Method to send email to us
+  def contactUs_email
+
+  end
+
+  # Method to send 'Thank You' email to visitor
+  def thankYou_email
+
+  end
+
+end
