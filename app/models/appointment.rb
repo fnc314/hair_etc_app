ecc86556d4927453f2e72a58b9f7fd98ab4a94dd
Franco N. Colaizzi <FrancoNColaizzi@fnc314.local>
2014-11-19 00:14:50 -0500

    moved seeds.rb logic to models and rewrote seeds.rb to be more clear (will be rewriting more to make process cleaner and file more maintainable).  May need to rework Offerings table to include price as String and a 'Group' column of type String to sort and create the same JSON file used in angular site

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,4 +3,36 @@ class Appointment < ActiveRecord::Base
   has_many :offerings, through: :appoffs
   belongs_to :client
   belongs_to :stylist
+
+  # Instance Methods used in hair_etc_app/db/seeds.rb
+
+  # Is called to assign 'random' a `appt_date_time` value to a given appointment
+  def seed_date_maker()
+    year = (DateTime.now.year-2..DateTime.now.year+2).to_a.sample
+    hour = (9..20).to_a.sample
+    minute = [00, 30].sample
+    month = (1..12).to_a.sample
+    if month == 2
+      day = (1..28).to_a.sample
+    elsif [1,3,5,7,8,10,12].include?(month)
+      day = (1..31).to_a.sample
+    else
+      day = (1..30).to_a.sample
+    end
+    self.appt_date_time = DateTime.new(year, month, day, hour, minute)
+  end
+
+  # Is called to assign 'random' `offerings` to a given appointment
+  def seed_offering_assignment()
+    off1, off2, off3 = Offering.all.sample, Offering.all.sample, Offering.all.sample
+    if off1 != off2 && off1 != off3 && off3 != off2
+      self.offerings = [off1, off2, off3]
+    elsif off1 != off2 && off2 != off3
+      self.offerings = [off1, off2]
+    else
+      new_off_id = ( (off1.id + off2.id + off3.id) / 3.0).round
+      self.offerings = [Offering.find(new_off_id)]
+    end
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,98 +1,90 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-Stylist.all.each do |s|
-  s.destroy
+# To preserve prestine ID values in ActiveRecord, one must run the following:
+# ```bash
+# ~$ rake db:drop
+# ~$ rake db:create
+# ~$ rake db:migrate
+# ~$ rake db:seed
+# ```
+
+def create_stylists()
+  puts "Creating Stylists"
+  Stylist.create(first_name: "Theresa", last_name: "Colaizzi", years: 25, title: "Owner/Stylist", 
+    specialties: ["Corrective Color", "Men's Styles"], schedule: ["Wednesday", "Thursday", "Saturday"], 
+    tag_line: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc finibus enim nisl, dictum semper arcu ornare viverra.", 
+    profile_pic: "https://s3.amazonaws.com/HairEtcPittsburgh/images/stylists/Theresa.png")
+  Stylist.create(first_name: "Stacy (Carolyn)", last_name: "Funwella", years: 43, 
+    specialties: ["Perms"], schedule: ["Monday", "Thursday", "Friday", "Saturday"], 
+    tag_line: "Let me make your locks curly.", 
+    profile_pic: "https://s3.amazonaws.com/HairEtcPittsburgh/images/stylists/Stacy.png")
+  Stylist.create(first_name: "Krista", last_name: "Vrana", years: 8, 
+    specialties: ["Fantasy Color"], schedule: ["Monday", "Friday", "Saturday"], 
+    tag_line: "I will fulfill your hair fantasy.", 
+    profile_pic: "https://s3.amazonaws.com/HairEtcPittsburgh/images/stylists/Krista.png")
+  Stylist.create(first_name: "Juneau", last_name: "Bowman", years: 32, 
+    specialties: ["Color"], schedule: ["Tuesday", "Wednesday", "Saturday"], 
+    tag_line: "I wanted to make the world beautiful so I became a stylist.", 
+    profile_pic: "https://s3.amazonaws.com/HairEtcPittsburgh/images/stylists/Juneau.png")
+  Stylist.create(first_name: "Chrissy", last_name: "Darnley-West", years: 25, 
+    specialties: ["HiLites", "UpDo", "Make-Up Artistry"], schedule: ["Tuesday", "Wednesday", "Thursday", "Saturday"], 
+    tag_line: "Help me help you find your inner beauty!", 
+    profile_pic: "https://s3.amazonaws.com/HairEtcPittsburgh/images/stylists/Chrissy.png")
+  Stylist.create(first_name: "Sheri", last_name: "Humbert", years: 23, 
+    specialties: ["Long-Hair Services", "Formal Styling"], schedule: ["Friday", "Saturday"], 
+    tag_line: "Let me inspire you.", 
+    profile_pic: "https://s3.amazonaws.com/HairEtcPittsburgh/images/stylists/Sheri.png")
 end
 
-Stylist.create(first_name: "Theresa", last_name: "Colaizzi", years: 25, title: "Owner/Stylist", 
-  specialties: ["Corrective Color", "Men's Styles"], schedule: ["Wednesday", "Thursday", "Saturday"], 
-  tag_line: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc finibus enim nisl, dictum semper arcu ornare viverra.", 
-  profile_pic: "https://s3.amazonaws.com/HairEtcPittsburgh/images/stylists/Theresa.png")
-Stylist.create(first_name: "Stacy (Carolyn)", last_name: "Funwella", years: 43, 
-  specialties: ["Perms"], schedule: ["Monday", "Thursday", "Friday", "Saturday"], 
-  tag_line: "Let me make your locks curly.", 
-  profile_pic: "https://s3.amazonaws.com/HairEtcPittsburgh/images/stylists/Stacy.png")
-Stylist.create(first_name: "Krista", last_name: "Vrana", years: 8, 
-  specialties: ["Fantasy Color"], schedule: ["Monday", "Friday", "Saturday"], 
-  tag_line: "I will fulfill your hair fantasy.", 
-  profile_pic: "https://s3.amazonaws.com/HairEtcPittsburgh/images/stylists/Krista.png")
-Stylist.create(first_name: "Juneau", last_name: "Bowman", years: 32, 
-  specialties: ["Color"], schedule: ["Tuesday", "Wednesday", "Saturday"], 
-  tag_line: "I wanted to make the world beautiful so I became a stylist.", 
-  profile_pic: "https://s3.amazonaws.com/HairEtcPittsburgh/images/stylists/Juneau.png")
-Stylist.create(first_name: "Chrissy", last_name: "Darnley-West", years: 25, 
-  specialties: ["HiLites", "UpDo", "Make-Up Artistry"], schedule: ["Tuesday", "Wednesday", "Thursday", "Saturday"], 
-  tag_line: "Help me help you find your inner beauty!", 
-  profile_pic: "https://s3.amazonaws.com/HairEtcPittsburgh/images/stylists/Chrissy.png")
-Stylist.create(first_name: "Sheri", last_name: "Humbert", years: 23, 
-  specialties: ["Long-Hair Services", "Formal Styling"], schedule: ["Friday", "Saturday"], 
-  tag_line: "Let me inspire you.", 
-  profile_pic: "https://s3.amazonaws.com/HairEtcPittsburgh/images/stylists/Sheri.png")
-
-Offering.all.each do |s|
-  s.destroy
+def create_offerings()
+  puts "Creating Offerings"
+  Offering.create(name: "Cut/Style", price: 20.00)
+  Offering.create(name: "Color", price: 50.00)
+  Offering.create(name: "Partial Color", price: 35.00)
+  Offering.create(name: "Hilights", price: 50.00)
+  Offering.create(name: "Partial Hilights", price: 35.00)
+  Offering.create(name: "Wax", price: 10.00)
+  Offering.create(name: "Updo", price: 60.00)
+  Offering.create(name: "Perm", price: 50.00)
+  Offering.create(name: "Brazilian Blowout", price: 80.00)
+  Offering.create(name: "Extensions", price: 30.00)
 end
 
-Offering.create(name: "Cut/Style", price: 20.00)
-Offering.create(name: "Color", price: 50.00)
-Offering.create(name: "Partial Color", price: 35.00)
-Offering.create(name: "Hilights", price: 50.00)
-Offering.create(name: "Partial Hilights", price: 35.00)
-Offering.create(name: "Wax", price: 10.00)
-Offering.create(name: "Updo", price: 60.00)
-Offering.create(name: "Perm", price: 50.00)
-Offering.create(name: "Brazilian Blowout", price: 80.00)
-Offering.create(name: "Extensions", price: 30.00)
-
-
-Client.all.each do |c|
-  c.destroy
+def create_clients()
+  puts "Creating Clients"
+  Client.create(first_name: "John", last_name: "Smith", email: "abc@def.com", 
+    password: "1234567890", password_confirmation: "1234567890", phone: "0000000000")
+  Client.create(first_name: "Rebecca", last_name: "Jones", email: "example@example.com", 
+    password: "1234567890", password_confirmation: "1234567890", phone: "0000000000")
+  Client.create(first_name: "Frodo", last_name: "Baggins", email: "example@sample.com", 
+    password: "1234567890", password_confirmation: "1234567890", phone: "0000000000")
 end
 
-Client.create(first_name: "John", last_name: "Smith", email: "abc@def.com", password: "1234567890", password_confirmation: "1234567890", phone: "0000000000")
-Client.create(first_name: "Rebecca", last_name: "Jones", email: "example@example.com", password: "1234567890", password_confirmation: "1234567890", phone: "0000000000")
-Client.create(first_name: "Frodo", last_name: "Baggins", email: "example@sample.com", password: "1234567890", password_confirmation: "1234567890", phone: "0000000000")
-
-Appointment.all.each do |a|
-  a.destroy
-end
-
-(1..50).each do
-
-  month = (1..12).to_a.sample
-  if month == 2
-    day = (1..28).to_a.sample
-  elsif [1,3,5,7,8,10,12].include?(month)
-    day = (1..31).to_a.sample
-  else
-    day = (1..30).to_a.sample
+def create_appointments()
+  puts "Creating Appointments"
+  (1..50).each do
+    a = Appointment.new()
+    a.seed_date_maker() # Model Method
+    a.stylist_id = Stylist.all.sample.id
+    a.client_id = Client.all.sample.id
+    a.seed_offering_assignment() # Model Method
+    a.save
   end
-  hour = (9..20).to_a.sample
-  minute = [00, 30].sample
-  date = DateTime.new(2014, month, day, hour, minute)
-  stylist_id = (1..Stylist.count).to_a.sample
-  client_id = [1,2].sample
-  a = Appointment.new(appt_date_time: date, client_id: client_id, stylist_id: stylist_id)
-  off = (1..Offering.count).to_a
-  off1 = off.sample
-  off2 = off.sample
-  off3 = off.sample
-  if off1 != off2 && off1 != off3 && off3 != off2
-    a.offerings.push(Offering.find(off1))
-    a.offerings.push(Offering.find(off2))
-    a.offerings.push(Offering.find(off3))
-  elsif off1 != off2 && off2 != off3
-    a.offerings.push(Offering.find(off1))
-    a.offerings.push(Offering.find(off2))
-  else
-    offering = ((off1+off2+off3)/3.0).round
-    a.offerings.push(Offering.find(offering))
-  end
-  a.save
-
 end
+
+def create_all()
+  create_stylists()
+  create_offerings()
+  create_clients()
+  create_appointments()
+end
+
+create_all()
+
+
+
+
+
+
+
+
+
+
