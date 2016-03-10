cc2f830789f15b3dca374244dbef80ff0aeac460
Franco N. Colaizzi <fnc314@gmail.com>
2016-03-09 22:09:44 -0500

    environment settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,12 +73,13 @@ HairEtcApp::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => ENV["DOMAIN_NAME"],
+    :authentication => :plain,
+    :enable_starttls_auto => true,
+    :user_name => ENV["GMAIL_USERNAME"],
+    :password => ENV["GMAIL_PASSWORD"]
   }
   # ActionMailer Config
   # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
